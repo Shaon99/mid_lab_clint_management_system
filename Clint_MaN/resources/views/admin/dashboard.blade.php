5f9dabb5ee4b06ee637efaa90e4ab182d9a00451
Shaon Ahmed <51782102+Shaon99@users.noreply.github.com>
2021-03-23 23:21:13 +0600

    fix

--- Clint_MaN/resources/views/admin/dashboard.blade.php
+++ Clint_MaN/resources/views/admin/dashboard.blade.php
@@ -19,7 +19,7 @@
     <li><span>{{$LoggedUserInfo->name}}</span></li>
     </div>
 
- <div class="btn btn-danger my-2 my-sm-0 " style="margin-left:420px;">
+ <div class="btn btn-danger my-2 my-sm-0 " style="margin-left:288px;">
  
      <a class="text-light"style="text-decoration: none;" href="/logout">Logout</a>
    </div>
